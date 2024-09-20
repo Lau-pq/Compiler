3700b9c0512b55fdbcc2f48a4098de3d30b26c4d
Lpq <1322325183@qq.com>
2024-09-20 21:08:31 +0800

    feat: 实现了简单的词法分析器

---
+++ scripts/diff.sh
@@ -0,0 +1,2 @@
+python -u diff.py ../data/std/token.txt ../data/out/token.txt
+python -u diff.py ../data/std/old_symbol_table.txt ../data/out/old_symbol_table.txt
--- src/cn/edu/hitsz/compiler/lexer/LexicalAnalyzer.java
+++ src/cn/edu/hitsz/compiler/lexer/LexicalAnalyzer.java
@@ -4,6 +4,9 @@ import cn.edu.hitsz.compiler.NotImplementedException;
 import cn.edu.hitsz.compiler.symtab.SymbolTable;
 import cn.edu.hitsz.compiler.utils.FileUtils;
 
+import java.text.CharacterIterator;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.StreamSupport;
 
 /**
@@ -16,9 +19,13 @@ import java.util.stream.StreamSupport;
  */
 public class LexicalAnalyzer {
     private final SymbolTable symbolTable;
+    private String sourceCodeBuffer;
+    private List<Token> tokens;
+    private int position;
 
     public LexicalAnalyzer(SymbolTable symbolTable) {
         this.symbolTable = symbolTable;
+        this.tokens = new ArrayList<>();
     }
 
 
@@ -31,7 +38,7 @@ public class LexicalAnalyzer {
         // TODO: 词法分析前的缓冲区实现
         // 可自由实现各类缓冲区
         // 或直接采用完整读入方法
-        throw new NotImplementedException();
+        this.sourceCodeBuffer = FileUtils.readFile(path);
     }
 
     /**
@@ -40,7 +47,93 @@ public class LexicalAnalyzer {
      */
     public void run() {
         // TODO: 自动机实现的词法分析过程
-        throw new NotImplementedException();
+        position = 0;
+        int nowState = 0;
+        while(position < sourceCodeBuffer.length()) {
+            char ch = sourceCodeBuffer.charAt(position);
+            switch (nowState) {
+                case 0 -> {
+                    if (Character.isWhitespace(ch)) {
+                        position++;
+                    } else if (isLetterOrUnderscore(ch)) {
+                        nowState = 1;
+                    } else if (Character.isDigit(ch)) {
+                        nowState = 2;
+                    } else {
+                        nowState = 3;
+                    }
+                }
+                case 1 -> {
+                    processString();
+                    nowState = 0;
+                }
+                case 2 -> {
+                    processNumber();
+                    nowState = 0;
+                }
+                case 3 -> {
+                    processSymbol();
+                    nowState = 0;
+                }
+            }
+        }
+        tokens.add(Token.eof());
+    }
+
+    private void processString() {
+        StringBuilder wordbuilder = new StringBuilder();
+        while(position < sourceCodeBuffer.length()){
+            char ch = sourceCodeBuffer.charAt(position);
+            if (isLetterOrDigitOrUnderscore(ch)) {
+                wordbuilder.append(ch);
+                position++;
+            } else {
+                break;
+            }
+        }
+        String word = wordbuilder.toString();
+        if (TokenKind.isAllowed(word)){
+            tokens.add(Token.simple(TokenKind.fromString(word)));
+        } else {
+            tokens.add(Token.normal(TokenKind.fromString("id"), word));
+            System.out.println(word);
+            if (!symbolTable.has(word)) {
+                symbolTable.add(word);
+            }
+        }
+    }
+
+    private void processNumber() {
+        StringBuilder numberbuilder = new StringBuilder();
+        while(position < sourceCodeBuffer.length()){
+            char ch = sourceCodeBuffer.charAt(position);
+            if (Character.isDigit(ch)) {
+                numberbuilder.append(ch);
+                position++;
+            } else {
+                break;
+            }
+        }
+        tokens.add(Token.normal(TokenKind.fromString("IntConst"), numberbuilder.toString()));
+    }
+
+    private void processSymbol() {
+        StringBuilder symbolbuilder = new StringBuilder();
+        char ch = sourceCodeBuffer.charAt(position);
+        if (ch == ';') {
+            tokens.add(Token.simple(TokenKind.fromString("Semicolon")));
+        } else {
+            tokens.add(Token.simple(TokenKind.fromString(String.valueOf(ch))));
+        }
+        position++;
+    }
+
+    private boolean isLetterOrUnderscore(char ch) {
+        return Character.isLetter(ch) || ch == '_';
+    }
+
+    private boolean isLetterOrDigitOrUnderscore(char ch){
+        return Character.isLetterOrDigit(ch) || ch == '_';
     }
 
     /**
@@ -53,7 +146,7 @@ public class LexicalAnalyzer {
         // 词法分析过程可以使用 Stream 或 Iterator 实现按需分析
         // 亦可以直接分析完整个文件
         // 总之实现过程能转化为一列表即可
-        throw new NotImplementedException();
+        return tokens;
     }
 
     public void dumpTokens(String path) {
