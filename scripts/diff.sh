16ed4a82b8751ca4f523b519eea0d7bc8d5ca293
Lpq <1322325183@qq.com>
2024-10-11 16:08:45 +0800

    feat: 利用LR(1)分析法，实现了语法分析器

--- scripts/diff.sh
+++ scripts/diff.sh
@@ -1,2 +1,3 @@
 python -u diff.py ../data/std/token.txt ../data/out/token.txt
 python -u diff.py ../data/std/old_symbol_table.txt ../data/out/old_symbol_table.txt
+python -u diff.py ../data/std/parser_list.txt ../data/out/parser_list.txt
--- src/cn/edu/hitsz/compiler/parser/SyntaxAnalyzer.java
+++ src/cn/edu/hitsz/compiler/parser/SyntaxAnalyzer.java
@@ -2,13 +2,13 @@ package cn.edu.hitsz.compiler.parser;
 
 import cn.edu.hitsz.compiler.NotImplementedException;
 import cn.edu.hitsz.compiler.lexer.Token;
-import cn.edu.hitsz.compiler.parser.table.LRTable;
-import cn.edu.hitsz.compiler.parser.table.Production;
-import cn.edu.hitsz.compiler.parser.table.Status;
+import cn.edu.hitsz.compiler.parser.table.*;
 import cn.edu.hitsz.compiler.symtab.SymbolTable;
 
+import java.security.PrivateKey;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 //TODO: 实验二: 实现 LR 语法分析驱动程序
 
@@ -23,7 +23,10 @@ import java.util.List;
 public class SyntaxAnalyzer {
     private final SymbolTable symbolTable;
     private final List<ActionObserver> observers = new ArrayList<>();
-
+    private List<Token> tokenList = new ArrayList<>();
+    private LRTable lrTable;
+    private Stack<Status> statusStack = new Stack<>();
+    private Stack<Symbol> symbolStack = new Stack<>();
 
     public SyntaxAnalyzer(SymbolTable symbolTable) {
         this.symbolTable = symbolTable;
@@ -79,14 +82,16 @@ public class SyntaxAnalyzer {
         // 你可以自行选择要如何存储词法单元, 譬如使用迭代器, 或是栈, 或是干脆使用一个 list 全存起来
         // 需要注意的是, 在实现驱动程序的过程中, 你会需要面对只读取一个 token 而不能消耗它的情况,
         // 在自行设计的时候请加以考虑此种情况
-        throw new NotImplementedException();
+        for (Token token: tokens) {
+            tokenList.add(token);
+        }
     }
 
     public void loadLRTable(LRTable table) {
         // TODO: 加载 LR 分析表
         // 你可以自行选择要如何使用该表格:
         // 是直接对 LRTable 调用 getAction/getGoto, 抑或是直接将 initStatus 存起来使用
-        throw new NotImplementedException();
+        lrTable = table;
     }
 
     public void run() {
@@ -94,6 +99,40 @@ public class SyntaxAnalyzer {
         // 你需要根据上面的输入来实现 LR 语法分析的驱动程序
         // 请分别在遇到 Shift, Reduce, Accept 的时候调用上面的 callWhenInShift, callWhenInReduce, callWhenInAccept
         // 否则用于为实验二打分的产生式输出可能不会正常工作
-        throw new NotImplementedException();
+        statusStack.push(lrTable.getInit());
+        symbolStack.push(new Symbol(Token.eof()));
+
+        for (int position = 0; position < tokenList.size();) {
+            Status currentStatus = statusStack.peek();
+            Token currentToken = tokenList.get(position);
+            Action currentAction = lrTable.getAction(currentStatus, currentToken);
+
+            switch (currentAction.getKind()) {
+                case Shift -> {
+                    callWhenInShift(currentStatus, currentToken);
+                    statusStack.push(currentAction.getStatus());
+                    symbolStack.push(new Symbol(currentToken));
+                    position++;
+                }
+                case Reduce -> {
+                    Production production = currentAction.getProduction();
+                    callWhenInReduce(currentStatus, production);
+                    for (Term term: production.body()){
+                        statusStack.pop();
+                        symbolStack.pop();
+                    }
+                    NonTerminal nonTerminal = production.head();
+                    Status gotoStatus = lrTable.getGoto(statusStack.peek(), nonTerminal);
+                    statusStack.push(gotoStatus);
+                    symbolStack.push(new Symbol(nonTerminal));
+                }
+                case Error -> throw new RuntimeException("Error");
+
+                case Accept -> {
+                    callWhenInAccept(currentStatus);
+                    return;
+                }
+            }
+        }
     }
 }
